f2dae47e5e972d1f5c6df671006ef3695eff77e0
Alex Oxford <alex@oakhaven.org>
2018-08-22 00:07:56 +0000

    Update deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-git pull "https://github.com/alexoxford/pslt-website.git" release
-workon django2
+git pull origin release
 python manage.py collectstatic
 touch /var/www/www_mydomain_com_wsgi.py
